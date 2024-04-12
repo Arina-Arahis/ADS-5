202974909a601bbcfa1ab123d025ed35a8d9c9f1
Arina-Arahis <159996104+Arina-Arahis@users.noreply.github.com>
2024-04-12 23:12:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,101 +1,101 @@
+// Copyright 2021 NNTU-CS
+#include <string>
 #include <map>
 #include "tstack.h"
 
-int Priora(char x) {
-  switch (x) {
-    case '(':
-      return 0;
-    case ')':
-      return 1;
-    case '+': case '-':
-      return 2;
-    case '*': case '/':
-      return 3;
-    default:
-      return -1;
-    }
-}
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
-  std::string rez, rez1;
-  TStack<char, 100>stack1;
-  for (auto& x : inf) {
-    int p = Priora(x);
-    if (p == -1) {
-      rez = rez + x + ' ';
-    } else {
-      char elem = stack1.get();
-      if (p == 0 || Priora(elem) < p || stack1.isEmpty()) {
-        stack1.push(x);
-      } else {
-        if (x == ')') {
-          while (Priora(elem) >= p) {
-            rez = rez + elem + ' ';
-            stack1.pop();
-            elem = stack1.get();
-          }
-          stack1.pop();
-        } else {
-          while (Priora(elem) >= p) {
-            rez = rez + elem + ' ';
-            stack1.pop();
-            elem = stack1.get();
-          }
-          stack1.push(x);
-        }
-      }
-    }
-  }
-  while (!stack1.isEmpty()) {
-    rez = rez + stack1.get() + ' ';
-    stack1.pop();
-  }
-  for (int i = 0; i < rez.size() - 1; i++)
-    rez1 += rez[i];
-  return rez1;
+bool isDigit(char b) {
+return (b >= '0' && b <= '9');
 }
 
-int schet(const int& p, const int& v, const int& x) {
-  switch (x) {
-    case '+':
-      return p + v;
-    case '-':
-      return p - v;
-    case '/':
-      return p / v;
-    case '*':
-      return p * v;
-    default:
-      return 0;
-  }
+bool isOperator(char z) {
+return (z == '+' || z == '-' || z == '*' || z == '/' ||  z == '(' || z == ')');
 }
+
+int prioritet(char v) {
+    if (v == '+' || v == '-')
+    return 1;
+    if (v == '*' || v == '/')
+    return 2;
+return 0;
+}
+
+
+std::string infx2pstfx(std::string inf) {
+  // добавьте код
+    std::string post;
+    int count = 0;
+    TStack <char, 100> stack;
+    for (char c : inf) {
+        if (isDigit(c)) {
+            count++;
+            if (count == 1) {
+                post += c;
+                continue;
+            }
+            post = post + ' ' + c;
+        } else if (isOperator(c)) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (stack.isEmpty()) {
+                stack.push(c);
+            } else if (prioritet(c) > prioritet(stack.get())) {
+                stack.push(c);
+            } else if (c == ')') {
+                while (stack.get() != '(') {
+                    post = post + ' ' + stack.get();
+                    stack.pop();
+                }
+                stack.pop();
+            } else {
+                int a = prioritet(c);
+                int b = prioritet(stack.get());
+                while (!stack.isEmpty() && a <= b) {
+                    post = post + ' ' + stack.get();
+                    stack.pop();
+                }
+                stack.push(c);
+            }
+        }
+    }
+    while (!stack.isEmpty()) {
+        post = post + ' ' + stack.get();
+        stack.pop();
+    }
+return post;
+}
+
+
 
 int eval(std::string pref) {
   // добавьте код
-  return 0;
-  TStack<int, 100> stack1;
-  std::string rez = "";
-  for (int i = 0; i < pref.size(); i++) {
-    char elem = pref[i];
-    if (Priora(elem) == -1) {
-      if (pref[i] == ' ') {
-        continue;
-      } else if (isdigit(pref[i+1])) {
-        rez += pref[i];
-        continue;
-      } else {
-        rez += pref[i];
-        stack1.push(atoi(rez.c_str()));
-        rez = "";
-      }
-    } else {
-      int v = stack1.get();
-      stack1.pop();
-      int p = stack1.get();
-      stack1.pop();
-      stack1.push(schet(p, v, elem));
-    }
-  }
-  return stack1.get();
+    TStack <int, 100> stack;
+    for (char c : pref) {
+        if (isDigit(c)) {
+            stack.push(c - '0');
+        } else if (isOperator(c)) {
+            int a = stack.get();
+            stack.pop();
+            int b = stack.get();
+            stack.pop();
+            switch (c) {
+                case '+':
+                    stack.push(a + b);
+                    break;
+                case '-':
+                    stack.push(b - a);
+                    break;
+                case '*':
+                    stack.push(a * b);
+                    break;
+                case '/':
+                    stack.push(b / a);
+                    break;
+                default:
+                    continue;
+            }
+        } else {
+            continue;
+        }
+        }
+return stack.get();
 }
