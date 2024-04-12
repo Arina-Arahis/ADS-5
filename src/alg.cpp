04b98169c6426455963599f19a452bf208f73784
Arina-Arahis <159996104+Arina-Arahis@users.noreply.github.com>
2024-04-12 19:02:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,101 +1,99 @@
 #include <map>
 #include "tstack.h"
 
-int Pr(char w) {
-  switch (w) {
+int Priora(char symbol) {
+    switch (symbol) {
     case '(':
-      return 0;
+        return 0;
     case ')':
-      return 1;
+        return 1;
     case '+': case '-':
-      return 2;
+        return 2;
     case '*': case '/':
-      return 3;
+        return 3;
     default:
-      return -1;
+        return -1;
     }
-}
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
-  std::string rez0, rez01;
-  TStack<char, 100>stack1;
-  for (auto& x : inf) {
-    int pt = Pr(x);
-    if (pt == -1) {
-      rez0 = rez0 + x + ' ';
-    } else {
-      char elem = stack1.get();
-      if (pt == 0 || Pr(elem) < pt || stack1.isEmpty()) {
-        stack1.push(x);
-      } else {
-        if (x == ')') {
-          while (Pr(elem) >= pt) {
-            rez0 = rez0 + elem + ' ';
-            stack1.pop();
-            elem = stack1.get();
-          }
-          stack1.pop();
-        } else {
-          while (Pr(elem) >= pt) {
-            rez0 = rez0 + elem + ' ';
-            stack1.pop();
-            elem = stack1.get();
-          }
-          stack1.push(x);
-        }
-      }
-    }
-  }
-  while (!stack1.isEmpty()) {
-    rez0 = rez0 + stack1.get() + ' ';
-    stack1.pop();
-  }
-  for (int i = 0; i < rez.size() - 1; i++)
-    rez01 += rez0[i];
-  return rez01;
 }
 
-int schet(const int& pt, const int& vt, const int& x) {
-  switch (x) {
+std::string infx2pstfx(std::string inf) {
+    std::string postfix = "";
+    TStack<char, 100> stack;
+    for (auto& symbol : inf) {
+        int priority = Priora(symbol);
+        if (priority == -1) {
+            postfix += symbol + ' ';
+        } else {
+            char top = stack.get();
+            if (priority == 0 || (Priora(top) < priority && stack.isEmpty())) {
+                stack.push(symbol);
+            } else {
+                if (symbol == ')') {
+                    while (Priora(top) >= priority) {
+                        postfix += top + ' ';
+                        stack.pop();
+                        top = stack.get();
+                    }
+                    stack.pop();
+                } else {
+                    while (Priora(top) >= priority) {
+                        postfix += top + ' ';
+                        stack.pop();
+                        top = stack.get();
+                    }
+                    stack.push(symbol);
+                }
+            }
+        }
+    }
+    while (!stack.isEmpty()) {
+        postfix += stack.get() + ' ';
+        stack.pop();
+    }
+    std::string cleanPostfix = "";
+    for (int i = 0; i < postfix.size() - 1; i++)
+        cleanPostfix += postfix[i];
+    return cleanPostfix;
+}
+
+int schet(const int& operand1, const int& operand2, const int& operation) {
+    switch (operation) {
     case '+':
-      return pt + vt;
+        return operand1 + operand2;
     case '-':
-      return pt - vt;
+        return operand1 - operand2;
     case '/':
-      return pt / vt;
+        return operand1 / operand2;
     case '*':
-      return pt * vt;
+        return operand1 * operand2;
     default:
-      return 0;
-  }
+        return 0;
+    }
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
-  TStack<int, 100> stack1;
-  std::string rez0 = "";
-  for (int i = 0; i < pref.size(); i++) {
-    char elem = pref[i];
-    if (Pr(elem) == -1) {
-      if (pref[i] == ' ') {
-        continue;
-      } else if (isdigit(pref[i+1])) {
-        rez0 += pref[i];
-        continue;
-      } else {
-        rez0 += pref[i];
-        stack1.push(atoi(rez0.c_str()));
-        rez0 = "";
-      }
-    } else {
-      int vt = stack1.get();
-      stack1.pop();
-      int pt = stack1.get();
-      stack1.pop();
-      stack1.push(schet(pt, vt, elem));
+    TStack<int, 100> stack;
+    std::string result = "";
+    for (int i = 0; i < pref.size(); i++) {
+        char symbol = pref[i];
+        if (Priora(symbol) == -1) {
+            if (pref[i] == ' ') {
+                continue;
+            } else if (isdigit(pref[i + 1])) {
+                result += pref[i];
+                continue;
+            } else {
+                result += pref[i];
+                stack.push(atoi(result.c_str()));
+                result = "";
+            }
+        } else {
+            int operand2 = stack.get();
+            stack.pop();
+            int operand1 = stack.get();
+            stack.pop();
+            stack.push(schet(operand1, operand2, symbol));
+        }
     }
-  }
-  return stack1.get();
+    return stack.get();
 }
