711e638d63ea878ac68b4a6bd2e17e710eacdc9f
Arina-Arahis <159996104+Arina-Arahis@users.noreply.github.com>
2024-04-12 17:58:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,95 +1,100 @@
 #include <map>
 #include "tstack.h"
 
+static int get_pr(char el) {
+  if (el == '(') {
+    return 0;
+  } else if (el == ')') {
+    return 1;
+  } else if ((el == '+') || (el == '-')) {
+    return 2;
+  } else if ((el == '*') || (el == '/')) {
+    return 3;
+  } else {
+    throw "Unknown Symbol!";
+  }
+}
+
 std::string infx2pstfx(std::string inf) {
   // добавьте код
   return std::string("");
+  std::string outa = "";
+  TStack<char, 100> chars;
+  for (int i = 0; i < inf.length(); i++) {
+    if (isdigit(inf[i])) {
+      outa += inf[i];
+      if (i != inf.length() - 1) {
+        outa += " ";
+      }
+    } else if (inf[i] == '(') {
+      chars.push(inf[i]);
+    } else if (chars.isempty()) {
+      chars.push(inf[i]);
+    } else if (inf[i] == ')') {
+      while (true) {
+        if (chars.isempty()) {
+          break;
+        } else if (chars.check() == '(') {
+          break;
+        }
+        outa += chars.pop();
+        if (i != inf.length() - 1) {
+          outa += " ";
+        }
+      }
+      chars.pop();
+    } else if (get_pr(inf[i]) > get_pr(chars.check())) {
+      chars.push(inf[i]);
+    } else {
+      while (true) {
+        if (chars.isempty()) {
+          break;
+        } else if (!(get_pri(inf[i]) <= get_pr(chars.check()))) {
+          break;
+        }
+        outa += chars.pop();
+        if (i != inf.length() - 1) {
+          outa += " ";
+        }
+      }
+      chars.push(inf[i]);
+    }
+  }
+  while (!chars.isempty()) {
+    outa += " ";
+    outa += chars.pop();
+  }
+  return outa;
 }
 
-int getPriority(char op) {
-    if (op == '+' || op == '-') return 1;
-    if (op == '*' || op == '/') return 2;
-    return 0;
-}
-
-bool isOperator(char ch) {
-    return (ch == '+' || ch == '-' || ch == '*' || ch == '/');
-}
-
-int evaluate(std::string pref) {
+int eval(std::string pref) {
   // добавьте код
   return 0;
-}
-
-std::string infixToPostfix(std::string inf) {
-    std::string postfix;
-    TStack<char, 100> stack;
-    for (char ch : inf) {
-        if (isdigit(ch)) {
-            postfix += ch;
-            postfix += ' ';
-        } else if (ch == '(') {
-            stack.push(ch);
-        } else if (isOperator(ch)) {
-            while (!stack.isEmpty() && getPriority(stack.get()) >= getPriority(ch)) {
-                postfix += stack.get();
-                postfix += ' ';
-                stack.pop();
-            }
-            stack.push(ch);
-        } else if (ch == ')') {
-            while (!stack.isEmpty() && stack.get() != '(') {
-                postfix += stack.get();
-                postfix += ' ';
-                stack.pop();
-            }
-            stack.pop();
-        }
+  std::string time = "";
+  TStack<int, 100> ints;
+  for (int i = 0; i < pref.length(); i++) {
+    if (isdigit(pref[i])) {
+      time += pref[i];
+    } else if (time.length() && pref[i] == ' ') {
+      ints.push(atoi(time.c_str()));
+      time = "";
+    } else if (pref[i] == '+') {
+      int two = ints.pop();
+      int one = ints.pop();
+      ints.push(one + two);
+    } else if (pref[i] == '-') {
+      int two = ints.pop();
+      int one = ints.pop();
+      ints.push(one - two);
+    } else if (pref[i] == '*') {
+      int two = ints.pop();
+      int one = ints.pop();
+      ints.push(one * two);
+    } else if (pref[i] == '/') {
+      int two = ints.pop();
+      int one = ints.pop();
+      ints.push(one / two);
     }
-    while (!stack.isEmpty()) {
-        postfix += stack.get();
-        postfix += ' ';
-        stack.pop();
-    }
-    if (!postfix.empty()) {
-        postfix.pop_back();
-    }
-    return postfix;
-}
-
-int evaluate(std::string post) {
-    TStack<int, 100> stack;
-    std::string number;
-    for (char ch : post) {
-        if (isdigit(ch)) {
-            number += ch;
-        } else if (!number.empty()) {
-            stack.push(std::atoi(number.c_str()));
-            number.clear();
-        }
-        if (isOperator(ch)) {
-            int operand2 = stack.get();
-            stack.pop();
-            int operand1 = stack.get();
-            stack.pop();
-            switch (ch) {
-                case '+':
-                    stack.push(operand1 + operand2);
-                    break;
-                case '-':
-                    stack.push(operand1 - operand2);
-                    break;
-                case '*':
-                    stack.push(operand1 * operand2);
-                    break;
-                case '/':
-                    stack.push(operand1 / operand2);
-                    break;
-            }
-        }
-    }
-    if (!number.empty()) {
-        stack.push(std::atoi(number.c_str()));
-    }
-    return stack.get();
+  }
+  return ints.pop();
 }
