a8228769e7383abd9b46552d6a28b57025105bdc
Arina-Arahis <159996104+Arina-Arahis@users.noreply.github.com>
2024-04-12 18:12:04 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,43 +1,44 @@
-#ifndef TSTACK_H
-#define TSTACK_H
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
 
 template<typename T, int maxsize>
 class TStack {
-private:
-    T data[maxsize];
-    int top;
+ private:
+  T data[maxsize];
+  int top;
 
-public:
-    TStack() {
-        top = -1;
-    }
+ public:
+  TStack() {
+    top = -1;
+  }
 
-    bool isEmpty() {
-        return (top == -1);
-    }
+  bool isEmpty() {
+    return (top == -1);
+  }
 
-    bool isFull() {
-        return (top == maxsize - 1);
-    }
+  bool isFull() {
+    return (top == maxsize - 1);
+  }
 
-    void push(T val) {
-        if (!isFull()) {
-            top++;
-            data[top] = val;
-        }
+  void push(T val) {
+    if (!isFull()) {
+      top++;
+      data[top] = val;
     }
+  }
 
-    void pop() {
-        if (!isEmpty()) {
-            top--;
-        }
+  void pop() {
+    if (!isEmpty()) {
+      top--;
     }
+  }
 
-    T get() {
-        if (!isEmpty()) {
-            return data[top];
-        }
+  T get() {
+    if (!isEmpty()) {
+      return data[top];
     }
+    return T();
+  }
 };
 
-#endif
+#endif  // INCLUDE_TSTACK_H_
