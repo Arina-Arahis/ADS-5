07952cf0869abe35541169d1aef28cc26826eaef
Arina-Arahis <159996104+Arina-Arahis@users.noreply.github.com>
2024-04-12 17:24:52 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -5,6 +5,35 @@
 template<typename T, int size>
 class TStack {
   // добавьте код стека
+ private:
+    T stackArray[100];
+    int topIndex;
+
+ public:
+    TStack() : topIndex(-1) {}
+    void push(T value) {
+        if (!isFull())
+            stackArray[++topIndex] = value;
+        else
+            throw std::string("Stack is full");
+    }
+    T get() const {
+        return stackArray[topIndex];
+    }
+    T pop() {
+        if (isEmpty()) {
+            throw std::string("Stack is empty");
+        } else {
+            return stackArray[topIndex--];
+        }
+    }
+    bool isEmpty() const {
+        return topIndex == -1;
+    }
+    bool isFull() const {
+        return topIndex == StackSize - 1;
+    }
+};
 };
 
 #endif  // INCLUDE_TSTACK_H_
